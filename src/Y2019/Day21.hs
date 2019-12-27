58ab7bd463c4a5766e2dfae4c853dc350755b03d
Jon Sangster <jon@ertt.ca>
2019-12-27 01:31:27 -0800

    Day21

---
+++ src/Y2019/Day21.hs
@@ -0,0 +1,108 @@
+module Y2019.Day21 (parts) where
+
+import Data.Char
+import Util.InstructionSet
+import Util.Program hiding (Run)
+
+
+
+parts :: [((String -> IO String), Maybe String)]
+parts = [ (part1, Just "19357761")
+        , (part2, Just "1142249706")
+        ]
+
+
+part1 input = do prog   <- program input
+                 status <- runExceptT $ runScript script prog
+                 case status of
+                   Left  video   -> return video
+                   Right hullDmg -> return $ show hullDmg
+  where
+    -- Jump if hole at 1 or 3, but not 4
+    script =
+      [ not' 1 J
+      , not' 3 T
+      , Or   T J
+      , and' 4 J
+      , Walk
+      ]
+
+
+part2 input = do prog   <- program input
+                 status <- runExceptT $ runScript script prog
+                 case status of
+                   Left  video   -> return video
+                   Right hullDmg -> return $ show hullDmg
+  where
+    script =
+      [ not'  1 J
+      , not'  2 T
+      , Or    T J
+      , not'  3 T
+      , Or    T J -- J = ~1 | ~2 | ~3                   : a reason to jump
+      , and'  4 J -- J = (~1 | ~2 | ~3) & 4             : a place to land
+      , not'  8 T
+      , flip' T
+      , or'   5 T -- T = 5 | 8
+      , And   T J -- J = ((~1 | ~2 | ~3) & 4) & (5 | 8) : space to walk or jump after
+      , Run
+      ]
+
+
+data Inst
+  = Walk
+  | Run
+  | And Reg Reg
+  | Or  Reg Reg
+  | Not Reg Reg
+
+
+instance Show Inst where
+  show Walk      = "WALK"
+  show Run       = "RUN"
+  show (And a b) = "AND "++show a++" "++show b
+  show (Or  a b) = "OR " ++show a++" "++show b
+  show (Not a b) = "NOT "++show a++" "++show b
+
+
+data Reg = T | J
+         | A | B | C | D | E | F | G | H | I deriving (Eq, Show)
+
+
+-- Some pointless helpers for readability
+
+and'  a b = And (toReg a) b
+or'   a b = Or  (toReg a) b
+not'  a b = Not (toReg a) b
+flip' a   = Not a a
+
+toReg 1 = A
+toReg 2 = B
+toReg 3 = C
+toReg 4 = D
+toReg 5 = E
+toReg 6 = F
+toReg 7 = G
+toReg 8 = H
+toReg 9 = I
+
+encode = map ord
+decode = map chr
+
+
+runScript :: [Inst] -> Program' -> ExceptT String IO Data
+runScript script p = do
+    results <- liftIO $ executeUntilHalt p (encode $ command script)
+    if any (> ord maxBound) results
+      then return $ last   results
+      else throwE $ decode results
+
+
+command xs =
+    if length xs <= 15
+      then concat $ (++"\n") . show <$> xs
+      else error "too many instructions"
+
+
+program :: String -> IO (Program')
+program = (fmap $ Program aoc19Set) . parseRAM
