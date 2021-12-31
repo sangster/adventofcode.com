d64f45d9cc6cc69b53e8a6273041b86de1708e3f
Jon Sangster <jon@ertt.ca>
2021-12-31 02:53:47 -0800

    Day 2021-12-23: part 2

--- src/Y2021/Day23.hs
+++ src/Y2021/Day23.hs
@@ -1,61 +1,37 @@
-{-# LANGUAGE DeriveAnyClass, DeriveGeneric, ImportQualifiedPost, NamedFieldPuns, TupleSections #-}
+{-# LANGUAGE ImportQualifiedPost, NamedFieldPuns, TupleSections #-}
 module Y2021.Day23 (parts) where
 
-import Data.Bool
-import Data.HashMap.Strict qualified as M
-import Data.HashSet qualified as S
-import Data.Hashable
-import Data.List
-import Data.Maybe
+import Data.Bool            (bool)
+import Data.Function        (on)
+import Data.HashMap.Strict  qualified as M
+import Data.Hashable        (Hashable, hashWithSalt, hashUsing)
+import Data.List            (nub, sort, (\\))
+import Data.Maybe           (fromJust)
 import Data.PQueue.Prio.Min qualified as Q
-import Data.Vector qualified as V
+import Data.Vector          qualified as V
 import Parser
 import Util.CharMap2D
-import Data.Function (on)
-
-import Debug.Trace
 
 
 parts = ( (part1, Just "16506")
-        , (part2, Nothing)
-        , mapToBurrow . parse' (map2dParser cell) --  . const testInput1
+        , (part2, Just "48304")
+        , mapToBurrow . parseAndFix (map2dParser cell)
         )
 
 
-testInput1 = unlines [ "#############"
-                     , "#...........#"
-                     , "###B#C#B#D###"
-                     , "  #A#D#C#A#"
-                     , "  #########"
-                     ]
--- One Solution: 12521
-testInput1Solution = [ (B, (6,1), (3,0))
-                     , (C, (4,1), (6,1))
-                     , (D, (4,2), (5,0))
-                     , (B, (3,0), (4,2))
-                     , (B, (2,1), (4,1))
-                     , (D, (8,1), (7,0))
-                     , (A, (8,2), (9,0))
-                     , (D, (7,0), (8,2))
-                     , (D, (5,0), (8,1))
-                     , (A, (9,0), (2,1))
-                     ]
-
-
+-- TODO: Slow
 part1 :: Burrow -> String
-part1 b = show $ fromJust $ findLeastEnergy b
-               $ Search (Q.singleton (0,0) (b, [])) S.empty Nothing
+part1 b = show . fromJust . findLeastEnergy $ newSearch b
 
 
--- low: 20866
+-- TODO: Slow
 part2 :: Burrow -> String
-part2 b = show $ findLeastEnergy b'
-               $ Search (Q.singleton (0,0) (b, [])) S.empty Nothing
+part2 b = show . fromJust . findLeastEnergy $ newSearch b'
   where
     b' = unfoldBurrow $ extract foldedPart
     (dx, dy) = head . sort $ roomCoords b
 
-    foldedPart = parse' (map2dParser cell) extraLines
+    foldedPart = parseAndFix (map2dParser cell) extraLines
     extraLines = unlines [ "  #D#C#B#A#"
                          , "  #D#B#A#C#"
                          ]
@@ -67,7 +43,8 @@ part2 b = show $ findLeastEnergy b'
                         }
       where
         depth'      = depth b + mapHeight foldedPart
-        amphipods'  = M.map (fmap (\(x,y) -> (x, bool y depth' $ y == depth b))) $ amphipods b
+        amphipods'  = fmap push `M.map` amphipods b
+        push (x,y)  = (x, bool y depth' $ y == depth b)
         amphipods'' = M.unionWith (++) amphipods' mid
         rooms       = sort $ (concat $ M.elems amphipods'')
 
@@ -80,27 +57,20 @@ part2 b = show $ findLeastEnergy b'
                              _             -> (acc, x')
 
 
-type History = [(Amphipod, MapCoord, MapCoord)]
-data Search = Search Queue (S.HashSet Burrow) (Maybe Energy)
-type Queue  = Q.MinPQueue (Int,Energy) (Burrow, [(Amphipod, MapCoord, MapCoord)])
+type SeenMap = M.HashMap Burrow Energy
+data Search = Search Queue SeenMap (Maybe Energy)
+type Queue  = Q.MinPQueue (Int,Energy) Burrow
 type Energy = Int
 type Map = CharMap2D Cell
 data Cell = Wall
           | Floor
-          | Room (Maybe Amphipod) deriving (Eq)
-
-instance Show Cell where
-  show Wall            = "#"
-  show Floor           = " "
-  show (Room (Just a)) = show a
-  show (Room Nothing)  = "."
+          | Room (Maybe Amphipod)
+          deriving (Eq, Show)
 
 
 data Amphipod = A | B | C | D deriving (Eq, Enum, Show)
 
 instance Hashable Amphipod where hashWithSalt = hashUsing fromEnum
-
-
 
 data Burrow = Burrow{ hallLength :: Int
                     , roomCols   :: (Int, Int, Int, Int)
@@ -110,31 +80,28 @@ data Burrow = Burrow{ hallLength :: Int
                     , depth      :: Int
                     }
 
-instance Show Burrow where
-  show Burrow{ amphipods, depth } =
-      "Burrow#"++show depth++" " ++ (intercalate "; " $ entry <$> [A,B,C,D])
-    where
-      entry amph = show amph ++ ":" ++ (intercalate "," $ show <$> amphipods M.! amph)
-
-
 instance Eq       Burrow where (==)         = (==) `on` amphipods
 instance Hashable Burrow where hashWithSalt = hashUsing amphipods
 
 
-findLeastEnergy :: Burrow -> Search -> Maybe Energy
+newSearch :: Burrow -> Search
+newSearch b = Search (Q.singleton (0,0) b) M.empty Nothing
 
-findLeastEnergy orig (Search queue seen prevBest) =
+
+findLeastEnergy :: Search -> Maybe Energy
+findLeastEnergy (Search queue seen prevBest) =
     case Q.minViewWithKey queue of
-      Just (((_,e), (b, history)), q) -> find' e b history q
-      Nothing                         -> prevBest
+      Just (((n,e), b), q) -> find' n e b q
+      Nothing              -> prevBest
   where
-    find' :: Energy -> Burrow -> History -> Queue -> Maybe Energy
-    find' e b history q
-        | S.member b seen = findLeastEnergy orig $ Search q  seen  prevBest
-        | isDone b        = findLeastEnergy orig $ Search q  seen  best
-        | otherwise       = findLeastEnergy orig $ Search q' seen' prevBest
+    find' :: Int -> Energy -> Burrow -> Queue -> Maybe Energy
+    find' n e b q
+        | seenCost <= e = findLeastEnergy $ Search q  seen  prevBest
+        | isDone b      = findLeastEnergy $ Search q  seen  best
+        | otherwise     = findLeastEnergy $ Search q' seen' prevBest
       where
-        seen' = S.insert b seen
+        seenCost = M.findWithDefault maxBound b seen
+        seen' = M.insert b e seen
         best = Just $ maybe e (\e' -> minimum [e, e']) prevBest
         opts = amphipodMoveOptions b
         q'   = foldr mkState q opts
@@ -142,12 +109,9 @@ findLeastEnergy orig (Search queue seen prevBest) =
         mkState (amph, from, xys) acc = foldr mkState' acc xys
           where
             mkState' xy acc' = bool acc'' acc' $ worseThanBest (e + cost)
-            -- mkState' xy acc' = acc'
               where
                 (cost, b') = move b amph from xy
-                acc'' = Q.insert (0 - inOwnRoomCount b', e + cost)
-                                 (b', (amph, from, xy):history)
-                                 acc'
+                acc'' = Q.insert (pred n, e + cost) b' acc'
 
     worseThanBest :: Energy -> Bool
     worseThanBest e = maybe False (e >) prevBest
@@ -178,23 +142,23 @@ energy D = 1000
 moveOptions :: Burrow -> Amphipod -> MapCoord -> [MapCoord]
 moveOptions b amph (x,y)
     | alreadyHome                       = []
-    | notInHall && someoneAbove (x,y)   = []
+    | inSomeRoom && someoneAbove (x,y)  = []
     | all (flip elem reachable) ownRoom = target
     | otherwise                         = reachable
   where
-    alreadyHome  = fromOwnRoom && all (flip elem fellowCoords) [(x,y') | y' <- [y .. depth b]]
-    fellowCoords = amphipods b M.! amph
-    notInHall    = not $ inHall (x,y)
+    alreadyHome  = fromOwnRoom && withFellows
     fromOwnRoom  = isOwnRoom b amph (x,y)
+    withFellows  = all (flip elem fellowCoords) [(x,y') | y' <- [y .. depth b]]
+    fellowCoords = amphipods b M.! amph
+    inSomeRoom   = not $ inHall (x,y)
     ownRoom      = ownRoomCoords b amph
-    allKindHome  = all (isOwnRoom b amph) fellowCoords
     someoneAbove = flip any occupied . isDirectlyAbove
     occupied     = occupiedCoords b
     reachable    = filter canReach emptySpaces
     target       = take 1 $ ownRoom \\ occupied
 
-    emptySpaces  = if notInHall -- && not fromOwnRoom
-                   then target ++ (((hallCoords b \\ entrances b) ++ ownRoom) \\ occupied)
+    emptySpaces  = if inSomeRoom
+                   then hallCoords b \\ occupied
                    else target
 
     canReach (x',y') = not $ or [hasAliens, roomBlocked, hallBlocked]
@@ -203,7 +167,12 @@ moveOptions b amph (x,y)
         roomBlocked    = someRoomDest && someoneAbove (x',y')
         hallBlocked    = any between $ filter inHall occupied
         between (xm,_) = xm > minimum [x,x'] && xm < maximum [x,x']
-        hasAliens      = someRoomDest && any (not . (== amph)) (roomOccupants b x')
+        hasAliens      = someRoomDest
+                      && any (not . (== amph)) (roomOccupants b x')
+
+
+isOwnRoom :: Burrow -> Amphipod -> MapCoord -> Bool
+isOwnRoom b a (x,y) = x == amphRoomCol b a && not (inHall (x,y))
 
 
 roomOccupants :: Burrow -> Int -> [Amphipod]
@@ -217,11 +186,8 @@ roomOccupants Burrow { amphipods } col = M.foldrWithKey f [] amphipods
 inOwnRoomCount :: Burrow -> Int
 inOwnRoomCount b = M.foldrWithKey count 0 (amphipods b)
   where
-    count amph xys acc = foldr (\xy acc' -> acc' + (bool 0 1 $ isOwnRoom b amph xy)) acc xys
-
-
-isOwnRoom :: Burrow -> Amphipod -> MapCoord -> Bool
-isOwnRoom b a (x,y) = x == amphRoomCol b a && not (inHall (x,y))
+    count  amph xys acc = foldr (atHome amph) acc xys
+    atHome amph xy acc' = acc' + bool 0 1 (isOwnRoom b amph xy)
 
 
 amphRoomCol :: Burrow -> Amphipod -> Int
@@ -229,10 +195,6 @@ amphRoomCol Burrow{ roomCols = (a,_,_,_) } A = a
 amphRoomCol Burrow{ roomCols = (_,b,_,_) } B = b
 amphRoomCol Burrow{ roomCols = (_,_,c,_) } C = c
 amphRoomCol Burrow{ roomCols = (_,_,_,d) } D = d
-
-
-entrances :: Burrow -> [MapCoord]
-entrances Burrow{ roomCols = (a,b,c,d) } = [(x,0) | x <- [a,b,c,d]]
 
 
 isDone :: Burrow -> Bool
@@ -262,10 +224,12 @@ isDirectlyAbove (x,y) (x',y') = x' == x && y' < y
 ownRoomCoords :: Burrow -> Amphipod -> [MapCoord]
 ownRoomCoords b amph = reverse $ (amphRoomCol b amph,) <$> [1 .. depth b]
 
+
 occupiedCoords :: Burrow -> [MapCoord]
 occupiedCoords = M.foldr (++) [] . amphipods
 
 
+-- | Convert the 2D ASCII map into a Burrow.
 mapToBurrow :: Map -> Burrow
 mapToBurrow m = Burrow{ hallLength = hall
                       , roomCols   = (a,b,c,d)
@@ -297,8 +261,8 @@ mapToBurrow m = Burrow{ hallLength = hall
 
 
 -- | The input text isn't perfectly square.
-parse' :: Parser Map -> String -> Map
-parse' p = fixLength . parse p
+parseAndFix :: Parser Map -> String -> Map
+parseAndFix p = fixLength . parse p
   where
     fixLength (m@CharMap2D{ mapCells = cs }) = m{ mapCells = mapCells' }
       where
