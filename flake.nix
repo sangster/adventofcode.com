4c67cd39a14e6fef29c633626314960b9d0b380d
Jon Sangster <jon@ertt.ca>
2022-12-01 07:14:01 -0800

    Update flake syntax

--- flake.nix
+++ flake.nix
@@ -1,123 +1,113 @@
 {
   description = "AdventOfCode runner";
-  inputs = {
-    flake-utils.url = github:numtide/flake-utils;
-    haskellNix.url = github:input-output-hk/haskell.nix;
-  };
+  inputs.flake-utils.url = github:numtide/flake-utils;
 
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+  outputs = { self, nixpkgs, flake-utils }:
     let
-      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-      flake = pkgs.aoc.flake {};
+      inherit (nixpkgs.lib) composeExtensions composeManyExtensions makeBinPath;
 
-      overlays = [ haskellNix.overlay
-        (final: prev: {
-          aoc =
-            final.haskell-nix.project' {
-              src = ./.;
-              compiler-nix-name = "ghc8104";
-              # This is used by `nix develop .` to open a shell for use with
-              # `cabal`, `hlint` and `haskell-language-server`
-              shell.tools = {
-                cabal = {};
-                hlint = {};
-                haskell-language-server = {};
-              };
-              # Non-Haskell shell tools go here
-              shell.buildInputs = with pkgs; [
-                hlint # Haskell Linter
-                nixpkgs-fmt # Format nix files
-              ];
-            };
-        })
-      ];
-    in flake // {
-      # Built by `nix build .`
-      defaultPackage = flake.packages."aoc:exe:aoc";
+      name = "aoc";
+      overlay = (_final: prev: rec {
+        aoc = prev.haskell.lib.justStaticExecutables haskellPackages.aoc;
 
-      apps = {
-        aoc-wait = pkgs.writeShellScriptBin "aoc-wait" ''
-          scriptName="$(basename "''${BASH_SOURCE[0]}")"
+        haskellPackages = prev.haskellPackages.override (old: {
+          overrides = composeExtensions
+            (old.overrides or (_: _: {}))
+            (_hfinal: hprev: {
+              aoc = hprev.developPackage { inherit name; root = self; };
+            });
+        });
+      });
+    in
+      { inherit overlay; } //
+      flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+        let
+          pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+        in {
+          packages.default = pkgs.aoc;
+          apps.aoc-wait = flake-utils.lib.mkApp {
+            drv = pkgs.writeShellScriptBin "aoc-wait" ''
+              scriptName="$(basename "''${BASH_SOURCE[0]}")"
 
-          usage() {
-            cat <<EOF
-          Usage: $scriptName [-hs] [-l FILE] [-t timeout] [arg...]
+              usage() {
+                cat <<EOF
+              Usage: $scriptName [-hs] [-l FILE] [-t timeout] [arg...]
 
-          Execute the application automatically upon file change.
+              Execute the application automatically upon file change.
 
-          Available options:
+              Available options:
 
-          -h, --help      Print this help and exit.
-          -l, --log       Copy STDOUT & STDERR to FILE.
-          -s, --start     Run once when first starting.
-          -t, --timeout   Kill app after N seconds.
-          EOF
-            exit
-          }
+              -h, --help      Print this help and exit.
+              -l, --log       Copy STDOUT & STDERR to FILE.
+              -s, --start     Run once when first starting.
+              -t, --timeout   Kill app after N seconds.
+              EOF
+                exit
+              }
 
-          opts() {
-            getopt -n "$scriptName" \
-                   -o hl:st: \
-                   -l help,log:,start,timeout: \
-                   -- "$@"
-          }
+              opts() {
+                getopt -n "$scriptName" \
+                       -o hl:st: \
+                       -l help,log:,start,timeout: \
+                       -- "$@"
+              }
 
-          parse_params() {
-            eval set -- "$(opts "$@")"
+              parse_params() {
+                eval set -- "$(opts "$@")"
 
-            # default values of variables set from params
-            at_start=0
-            timeout=90
-            logfile=""
+                # default values of variables set from params
+                at_start=0
+                timeout=90
+                logfile=""
 
-            while :; do
-              case "''${1-}" in
-              -h | --help) usage ;;
-              -s | --start) at_start=1 ;;
-              -l | --log) logfile="$2"; shift ;;
-              -t | --timeout) timeout="$2"; shift ;;
-              --) shift; break ;;
-              -?*) die "Unknown option: $1" ;;
-              *) break ;;
-              esac
-              shift
-            done
+                while :; do
+                  case "''${1-}" in
+                  -h | --help) usage ;;
+                  -s | --start) at_start=1 ;;
+                  -l | --log) logfile="$2"; shift ;;
+                  -t | --timeout) timeout="$2"; shift ;;
+                  --) shift; break ;;
+                  -?*) die "Unknown option: $1" ;;
+                  *) break ;;
+                  esac
+                  shift
+                done
 
-            args=("$@")
+                args=("$@")
 
-            return 0
-          }
+                return 0
+              }
 
-          parse_params "$@"
+              parse_params "$@"
 
-          function run() {
-            timeout "$timeout" stack run "''${args[@]}"
-            echo ---
-          }
+              function run() {
+                timeout "$timeout" stack run "''${args[@]}"
+                echo ---
+              }
 
-          function logRun() {
-            if [ -n "$logfile" ]; then
-              run 2>&1 | ${pkgs.coreutils}/bin/tee "$logfile"
-            else
-              run
-            fi
-          }
+              function logRun() {
+                if [ -n "$logfile" ]; then
+                  run 2>&1 | ${pkgs.coreutils}/bin/tee "$logfile"
+                else
+                  run
+                fi
+              }
 
-          [ $at_start -eq 1 ] && logRun
+              [ $at_start -eq 1 ] && logRun
 
-          ${pkgs.inotify-tools}/bin/inotifywait -m \
-              -q \
-              -e CLOSE_WRITE \
-              -r app \
-              -r inputs \
-              -r src \
-              --exclude '/[.#].*' \
-            | while read file; do
-               echo "inotify event: $file"
-               logRun
-            done
-        '';
-      };
-    });
+              ${pkgs.inotify-tools}/bin/inotifywait -m \
+                  -q \
+                  -e CLOSE_WRITE \
+                  -r app \
+                  -r inputs \
+                  -r src \
+                  --exclude '/[.#].*' \
+                | while read file; do
+                   echo "inotify event: $file"
+                   logRun
+                done
+            '';
+          };
+        }
+      );
 }
